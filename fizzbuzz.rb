17ceeab5d4d1ac04d7b3c5566ee88bbb6c7be222
Philip Sterling <philipssterlings@gmail.com>
2019-05-31 20:28:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
+  string = nil
   if(int%3 == 0)
-    "Fizz"
+    string += "Fizz"
   end
   if(int%5 == 0)
-    "Buzz"
+    string += "Buzz"
   end
+  return string
 end
